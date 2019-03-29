c526c9d6705a086e6eb89bf825dc40ab80c20e78
hainht88 <hainht88@gmail.com>
2019-03-30 02:34:24 +0700

    Update sidebar.jsx

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,7 +4,7 @@ import sidebarLogo from "../images/delight_prompt_2.png";
 const SideBar = () => {
   return (
     <div className="col-md-3">
-      <div className="card my-4">
+      <div className="card mb-4">
         <div className="card-body d-flex flex-column align-items-center">
           <img className="card-img-top" src={sidebarLogo} alt="Sidebar Logo" />
           <h5 className="card-title">Sneak a peek at the new Twitter</h5>
